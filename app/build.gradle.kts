e280369676d4ced1c9ca53bc75af235b0f56255d
ValSua <109840298+ValSua@users.noreply.github.com>
2024-05-09 23:38:24 -0500

    Se integra imagen dinamica de huella digital y autenticación con biometría

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +49,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.airbnb.android:lottie:3.7.0")
+    implementation("androidx.biometric:biometric:1.1.0")
+    implementation("androidx.biometric:biometric-ktx:1.2.0-alpha04")
 }
--- app/src/main/java/com/example/dogapp/MainActivity.kt
+++ app/src/main/java/com/example/dogapp/MainActivity.kt
@@ -1,19 +1,55 @@
 package com.example.dogapp
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
-import com.airbnb.lottie.LottieAnimationView
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import com.example.dogapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Obtener una referencia al ImageView
         val imageView: ImageView = findViewById(R.id.imageView)
 
         // Cargar la imagen en el ImageView
         imageView.setImageResource(R.drawable.head_48478_960_720)
+
+        val animationView = binding.animationView
+        animationView.setOnClickListener {
+            showBiometricDialog()
+        }
+
     }
+
+    private fun showBiometricDialog() {
+        val executor = ContextCompat.getMainExecutor(this)
+        val biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    // Autenticación exitosa, dirigir al usuario a la nueva actividad
+                    val intent = Intent(this@MainActivity, HelloWorldActivity::class.java)
+                    startActivity(intent)
+                }
+
+            })
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Autenticación con Biometría")
+            .setSubtitle("Ingrese su huella digital")
+            .setNegativeButtonText("Cancelar")
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+
 }
