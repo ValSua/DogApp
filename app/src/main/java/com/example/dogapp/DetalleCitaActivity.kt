b9891483b6ab6fb5ac5ccadfa384c2ee5a934932
brigittechavez <brigitte.chavez@correounivalle.edu.co>
2024-05-17 16:29:18 -0500

    Toolbar detalle cita

--- app/src/main/java/com/example/dogapp/DetalleCitaActivity.kt
+++ app/src/main/java/com/example/dogapp/DetalleCitaActivity.kt
@@ -1,11 +1,17 @@
-package com.example.dogapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.example.dogapp.databinding.ActivityDetalleCitaBinding
 
 class DetalleCitaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetalleCitaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detalle_cita)
+        binding = ActivityDetalleCitaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
